b1e6431096eb225fe0a390b7d5c1cb85ef5078e5
Gil Markham <ddimensia@gmail.com>
2022-03-09 22:11:25 -0800

    Work in progress, most of the stuff is there to auto install just about everything.

---
+++ dietpi_image_prep.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+MOUNT_POINT="/mnt/diskImage"
+SCRIPT_DIR=`dirname $0`
+
+if [ "$EUID" -ne 0 ] 
+then 
+	echo "Please run as root"
+       	exit
+fi
+
+if [ -z "$1" ]
+then
+	echo "No image name provided"
+	exit
+fi
+
+# Determine the start offset for the first bootable partition
+partition_offset=$(parted -s "$1" unit B print | grep -A 2 '^Number' | awk '$7~/boot/ {sub(/B$/,"",$2); print $2; exit;}')
+
+# Create a mount point if one doesn't exist
+[ ! -d "$MOUNT_POINT" ] && mkdir "$MOUNT_POINT"
+
+# Mount the partition
+echo "Mounting '$1' to '$MOUNT_POINT'"
+mount -o loop,offset=$partition_offset "$1" "$MOUNT_POINT"
+
+if [ -f "$MOUNT_POINT/cmdline.txt" ]
+then
+	echo "Modifying cmdline.txt"
+	sed -i "1 s/$/ consoleblank=0/" "$MOUNT_POINT/cmdline.txt"
+fi
+
+if [ -f "$MOUNT_POINT/config.txt" ]
+then
+	echo "Modifying config.txt"
+	sed -i "s/gpu_mem_1024=.*/gpu_mem_1024=256/" "$MOUNT_POINT/config.txt"
+	echo "dtoverlay=vc4-fkms-v3d" >> "$MOUNT_POINT/config.txt"
+fi
+
+if [ -f "$MOUNT_POINT/dietpi.txt" ]
+then
+	echo "Found dietpi.txt"
+	cp "$SCRIPT_DIR/dietpi.txt" "$MOUNT_POINT/dietpi.txt"
+	cp "$SCRIPT_DIR/rcdash_setup.sh" "$MOUNT_POINT/Automation_Custom_Script.sh"
+else
+       	echo "Image is missing dietpi.txt, verify if image is a DietPi image!"
+fi
+
+# Unmount image
+echo "Unmounting image"
+#umount "$MOUNT_POINT"
---
+++ rcdash_setup.sh
@@ -0,0 +1,159 @@
+#!/bin/bash -e
+
+if [ "$EUID" -ne 0 ] 
+then 
+	echo "Must be run as root"
+       	exit
+fi
+
+if [ -d /boot/dietpi ]
+then
+       	USER=dietpi
+else
+	USER=pi
+	sed -i '1 s/$/ logo.nologo consoleblank=0/' /boot/cmdline.txt
+	echo "disable-spash=1" >> /boot/config.txt
+	echo "gpu_mem=256" >> /boot/config.txt
+fi
+
+# Install the necessary dependencies for the RC App
+apt-get -y install mesa-utils libgles2 libegl1-mesa libegl-mesa0 mtdev-tools pmount python3-gpiozero
+
+# Setup wifi reconnect
+cat > /etc/cron.d/wifi_reconnect.cron <<EOF
+# Run the wifi_reconnect script every minute
+* *   * * *   root    /usr/local/bin/wifi_reconnect.sh
+EOF
+
+cat > /usr/local/bin/wifi_reconnect.sh <<EOF
+#!/bin/bash 
+ 
+SSID=$(/sbin/iwgetid --raw) 
+
+if [ -z "$SSID" ] 
+then 
+    echo "`date -Is` WiFi interface is down, trying to reconnect" >> /home/pi/wifi-log.txt
+    sudo ifconfig wlan0 down
+    sleep 30
+    sudo ifconfig wlan0 up 
+fi 
+
+echo "WiFi check finished"
+EOF
+
+chmod +x /usr/local/bin/wifi_reconnect.sh
+
+# Setup shutdown button support for GPIO21
+cat > /usr/local/bin/shutdown_button.py <<EOF
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# example gpiozero code that could be used to have a reboot
+#  and a shutdown function on one GPIO button
+# scruss - 2017-10
+
+use_button=21                       # lowest button on PiTFT+
+
+from gpiozero import Button
+from signal import pause
+from subprocess import check_call
+
+held_for=0.0
+
+def rls():
+    global held_for
+    if (held_for > 1.0):
+        check_call(['/sbin/reboot'])
+    else:
+        held_for = 0.0
+
+def hld():
+    # callback for when button is held
+    #  is called every hold_time seconds
+    global held_for
+    # need to use max() as held_time resets to zero on last callback
+    held_for = max(held_for, button.held_time + button.hold_time)
+    if (held_for > 4.0):
+        check_call(['/sbin/poweroff'])
+
+button=Button(use_button, hold_time=1.0, hold_repeat=True)
+button.when_held = hld
+button.when_released = rls
+
+pause() # wait forever
+EOF
+chmod +x /usr/local/bin/shutdown_button.py
+
+cat > /etc/systemd/system/shutdown_button.service <<EOF
+[Unit]
+Description=GPIO shutdown button
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/usr/bin/python3 /usr/local/bin/shutdown_button.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable shutdown_button.service
+systemctl start shutdown_button.service
+
+# Groups needed for the dietpi user to access opengl, input (touch/mouse) and usb serial ports
+adduser $USER render
+adduser $USER video
+adduser $USER input
+adduser $USER dialout
+
+# Add automount rules
+cat > /usr/local/bin/automount <<EOF
+#!/bin/bash
+ 
+PART=$1
+
+mount_points=(usb1 usb2 usb3 usb4 usb5)
+for i in "${mount_points[@]}"
+do
+    if ! mountpoint -q /media/$1
+    then
+        /usr/bin/pmount --umask 000 --noatime -w --sync /dev/${PART} /media/$i
+        exit 0
+    fi
+done
+EOF
+chmod +x /usr/local/bin/automount
+
+cat > /etc/udev/rules.d/usbstick.rules <<EOF
+ACTION=="add", KERNEL=="sd[a-z][0-9]", TAG+="systemd", ENV{SYSTEMD_WANTS}="usbstick-handler@%k"
+EOF
+
+cat > /lib/systemd/system/usbstick-handler@.service <<EOF
+[Unit]
+Description=Mount USB sticks
+BindsTo=dev-%i.device
+After=dev-%i.device
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/local/bin/automount %I
+ExecStop=/usr/bin/pumount /dev/%I
+EOF
+
+# Download and install the RC App
+cd /opt
+wget https://autosportlabs-software.s3-us-west-2.amazonaws.com/racecapture_linux_raspberrypi_2.2.1.tar.bz2
+tar xjf racecapture_linux_raspberrypi_2.2.1.tar.bz2
+cat > /home/pi/.bashrc <<EOF
+echo "Starting RaceCapture, Ctrl-c to abort!"
+for i in $(seq 5 -1 1)
+do
+echo -n $i
+sleep 1
+echo -ne '\b'
+done
+/opt/racecapture/run_racecapture.sh
+EOF
