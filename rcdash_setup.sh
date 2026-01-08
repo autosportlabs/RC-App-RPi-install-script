54eb077b1b8232734db4f6b1122305e8ba2efab3
Brent Picasso <brent@autosportlabs.com>
2026-01-07 20:31:00 -0800

    inline the launcher script to make it more self-contained

--- rcdash_setup.sh
+++ rcdash_setup.sh
@@ -13,6 +13,7 @@ fi
 CONFIG_TXT="$BOOT_BASE/config.txt"
 CMDLINE_TXT="$BOOT_BASE/cmdline.txt"
 #OFFICIAL_DISPLAY=`lsmod | grep -q edt_ft5x06 && echo 1 || echo 0`
+LAUNCHER_SCRIPT="/opt/racecapture/run_racecapture.sh"
 
 function eval_setting() {
 	if [ "$1" == "$2" ]; then
@@ -122,6 +123,68 @@ function add_group() {
 	if ! groups "$USER" | grep -qw "$groupname"; then
 		adduser $USER $groupname
 	fi
+}
+
+function install_launcher() {
+	cat > "$LAUNCHER_SCRIPT" <<-EOF
+#!/bin/bash
+
+# RaceCapture App launch script for Linux
+
+# launch with -w 1 to enable watchdog
+# launch with -l <logfile> to specify the logfile path
+
+
+LOGFILE=~/racecapture.log
+WATCHDOG=0
+
+while getopts ":w:l:" opt; do
+    case ${opt} in
+    w)  WATCHDOG=$OPTARG
+        ;;
+    l)  LOGFILE=$OPTARG
+        ;;
+    esac
+done
+shift $(expr $OPTIND - 1)
+APPARGS=$@
+
+# create the configuration directories as needed
+mkdir -p ~/.kivy
+mkdir -p ~/.config/racecapture
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Configure keyring to store Podium user credentials
+export PYTHON_KEYRING_BACKEND=sagecipher.keyring.Keyring
+if [ ! -d "${HOME}/.ssh" ]
+then
+# Create the ssh keys if they don't exist
+cat /dev/zero | ssh-keygen -q -N ""
+fi
+killall ssh-agent &>/dev/null
+eval "$(ssh-agent)" &>/dev/null
+ssh-add &>/dev/null
+
+# run and re-launch if crashed
+while
+  if [ -f $LOGFILE ]; then
+    mv $LOGFILE ${LOGFILE}_last
+  fi
+
+  ./race_capture $APPARGS >> $LOGFILE 2>&1
+
+  exitstatus=$?
+  if [[ $exitstatus -eq 0 || $WATCHDOG -ne 1 ]]; then
+    break
+  fi
+  echo "racecapture crashed with code $?. Restarting..." >> $LOGFILE
+do
+  :
+done
+	EOF
+
 }
 
 function setup_user() {
@@ -477,7 +540,8 @@ ExecStop=/usr/bin/pumount /dev/%I
 	fi
 
 	install_rc_app
-
+    install_launcher
+    
 	RC_SCRIPT_ARGS="-- -c graphics:show_cursor:0"
 	if [[ $MODE == "X11" ]]; then
 		RC_SCRIPT_ARGS+=" --size=$RESOLUTION"
@@ -536,6 +600,7 @@ $RC_LAUNCH_COMMAND
 	dump_settings
 else
 	install_rc_app
+	install_launcher
 fi
 
 echo "Installation complete!!"
